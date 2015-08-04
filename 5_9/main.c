8311d6bef0fff02db6919fd6672b969a9b390589
中软mini003 <zhongruanmini003@zhongruanmini003deMac-mini.local>
2015-08-04 11:21:53 +0800

    Initial Commit

---
+++ 5_9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  5_9
+//
+//  Created by 中软mini003 on 15/8/4.
+//  Copyright (c) 2015年 中软mini003. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
